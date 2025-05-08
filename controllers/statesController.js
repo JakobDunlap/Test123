c27a83adfb23c599c6145857f5014eef686788dc
Jakob Dunlap <jakob_dunlap@outlook.com>
2025-05-07 19:39:56 -0500

    try again

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -291,7 +291,10 @@ const createNewFunFact = async (req, res) => {
     const stateCode = req.params.state.toUpperCase();
     const funfacts = req.body.funfacts;
 
-    if (Object.keys(!funfacts)) {
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ 'message': 'State fun facts value required' });
+    }
+    if (funfacts.length === 0) {
         return res.status(400).json({ 'message': 'State fun facts value required' });
     }
     if (!Array.isArray(funfacts)) {
