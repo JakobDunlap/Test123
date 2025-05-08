1d7f7948778c97460355e2a9b34c948ee8818daf
Jakob Dunlap <jakob_dunlap@outlook.com>
2025-05-07 19:55:15 -0500

    fix post method

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -291,7 +291,14 @@ const createNewFunFact = async (req, res) => {
     const stateCode = req.params.state.toUpperCase();
     const funfacts = req.body.funfacts;
 
-    if (Object.keys(req.body).length === 0) {
+    // console.log(funfacts);
+    // const emptyarray = [];
+    // console.log(emptyarray);
+    // console.log(funfacts.length === 0);
+    if (!funfacts) {
+        return res.status(400).json({ 'message': 'State fun facts value required' });
+    }
+    if (funfacts.length === 0) {
         return res.status(400).json({ 'message': 'State fun facts value required' });
     }
     if (!Array.isArray(funfacts)) {
