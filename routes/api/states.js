e7794c7652dd366f9e71e5534cbf7e17974ba679
Jakob Dunlap <jakob_dunlap@outlook.com>
2025-05-08 19:19:07 -0500

    Attempting to implement middleware

--- routes/api/states.js
+++ routes/api/states.js
@@ -3,23 +3,19 @@ const router = express.Router();
 const statesController = require('../../controllers/statesController');
 const verifyStateCode = require('../../middleware/verifyStates');
 
-//CHANGECHANGECHANGECHANGECHANGECHANGECHANGE
-router.route('/')
+router.route('/', verifyStateCode)
     .get(statesController.getAllStates)
     .put(statesController.updateFunFact)
     .delete(statesController.deleteFunFact);
+
+router.route('/:state')
+    .get(statesController.getState);
 
 router.route('/:state/funfact')
     .post(statesController.createNewFunFact)
     .get(statesController.getFunFact)
     .patch(statesController.updateFunFact)
     .delete(statesController.deleteFunFact);
-
-// router.route('/:state/funfact')
-//     .get(statesController.getFunFact);
-
-// router.route('/:state/funfact')
-//     .delete(statesController.deleteFunFact);
 
 router.route('/:state/capital')
     .get(statesController.getStateCapital);
@@ -32,12 +28,5 @@ router.route('/:state/population')
 
 router.route('/:state/admission')
     .get(statesController.getStateAdmission);
-
-// router.route('/:state/funfact')
-//     .patch(statesController.updateFunFact);
-
-    //Currently verifystatecode is not working 
-router.route('/:state')
-    .get(statesController.getState);
 
 module.exports = router;
